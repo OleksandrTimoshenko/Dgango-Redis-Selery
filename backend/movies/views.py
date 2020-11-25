5653fb0fdcf7f209638b0636db980c59f0f31032
Oleksandr Timoshenko <o.s.tymoshenko1999@gmail.com>
2020-11-25 18:01:32 +0200

    merging with a movies project

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from movies.tasks import set_cache
 
-# Create your views here.
+import os
+import redis
+import json
+
+CACHE_KEY_NAME = 'cache:films'
+
+set_cache.delay()
+
+redis_client = redis.StrictRedis.from_url(
+    os.getenv('REDIS_URL'), decode_responses=True)
+
+films_json = redis_client.get(CACHE_KEY_NAME,)
+
+def index(request):
+    return redirect("/movies")
+
+def movies(request):
+    films = json.loads(films_json)
+    return render(request, "movies.html",  {'films':films})
