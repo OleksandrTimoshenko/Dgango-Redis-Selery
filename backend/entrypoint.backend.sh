87036ce40929c30184cd36a8d32113d893044953
Oleksandr Timoshenko <o.s.tymoshenko1999@gmail.com>
2020-11-30 12:43:37 +0200

    add tests to Django project source

--- backend/entrypoint.backend.sh
+++ backend/entrypoint.backend.sh
@@ -2,4 +2,5 @@
 python manage.py migrate
 # If you don't want to show the wait_for_it page you can wait for data from celery now
 #sleep 120
+python manage.py test
 python manage.py runserver 0.0.0.0:8000
---
+++ backend/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase, Client
+
+
+class TestGetSomething(TestCase):
+    def test_root_url(self):
+        path = "/"
+        client = Client()
+        response = client.get(path=path)
+        self.assertEqual(response.status_code, 302)
+
+    def test_movies_url(self):
+        path = "/movies"
+        client = Client()
+        response = client.get(path=path)
+        self.assertEqual(response.status_code, 301)
+        
